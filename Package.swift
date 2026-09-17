086191339b2f3a188b4d7c14922ef150b1da9023
ShashwatAnand-07 <shashwat.anand@finbox.in>
2026-09-17 14:29:22 +0530

    SDK-3147: Update SDK

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "RiskManager",
             url: "https://github.com/finbox-in/dc-ios-sdk/releases/download/v0.3.11/RiskManager-0.3.11.xcframework.zip",
-            checksum: "ec216ecdd6e428869e1cfe39f40dae623b3b4b79f72cd34c863b0ca4540a9bef"
+            checksum: "a6f144d71cffbbe09a7298fb6adc571e9a1bedb553fc6cac1f8bb993e3ce30cb"
         ),
     ]
 )
--- RiskManager.podspec
+++ RiskManager.podspec
@@ -19,7 +19,7 @@ Pod::Spec.new do |spec|
 
   spec.source = {
     :http => "https://github.com/finbox-in/dc-ios-sdk/releases/download/v0.3.11/RiskManager-0.3.11.xcframework.zip",
-    :sha256 => "ec216ecdd6e428869e1cfe39f40dae623b3b4b79f72cd34c863b0ca4540a9bef"
+    :sha256 => "a6f144d71cffbbe09a7298fb6adc571e9a1bedb553fc6cac1f8bb993e3ce30cb"
   }
 
   spec.vendored_frameworks = "RiskManager.xcframework"
